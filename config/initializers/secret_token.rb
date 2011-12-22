fee601571c3d4977771666517dcee0e1981fff93
Andy <andy.bell007@gmail.com>
2011-12-22 23:56:57 +0000

    new blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '1a539dddab8eed35ff9ab5a8bf796bac4dc046e79133b4caa0cf88f009776e1c448d5e3bf3d0a1ad77ee0be95eb202e3c166e3f7044f4cc61217b0958fe2c066'
