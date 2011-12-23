2130a573d29c62421fc74f09ae26a5fa50f240de
Andy <andy.bell007@gmail.com>
2011-12-23 17:45:28 +0000

    updated posts with tags and commnets

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ActiveRecord::Base
+ 
 validates_presence_of :name, :message => "- Write your name dickhead"
-validates :title, :presence=>true
+  validates_presence_of :title, :message => "- Write a title you cunt"
+  has_many :comments, :dependent => :destroy
+  has_many :tags
+ 
+  accepts_nested_attributes_for :tags, :allow_destroy => :true,
+    :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111223144042) do
+ActiveRecord::Schema.define(:version => 20111223172205) do
 
   create_table "comments", :force => true do |t|
     t.string   "commenter"
@@ -29,5 +29,14 @@ ActiveRecord::Schema.define(:version => 20111223144042) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "tags", :force => true do |t|
+    t.string   "name"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "tags", ["post_id"], :name => "index_tags_on_post_id"
 
 end
